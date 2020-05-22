2da275676f0bbfab8d8712b91ea32c65decea068
o0pmitev <pmitev89@gmail.com>
2020-05-22 17:42:30 +0200

    Updates

---
+++ precache-manifest.ae9fd6bb2d357ce654eec9a0f15f2b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa5231ab488bb8490582ff82517d570",
+    "url": "/reactTest2/index.html"
+  },
+  {
+    "revision": "6ac2032eb89c775ed0cf",
+    "url": "/reactTest2/static/css/main.afaa0373.chunk.css"
+  },
+  {
+    "revision": "6a8055ec3164e3493660",
+    "url": "/reactTest2/static/js/2.e3ab4c13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTest2/static/js/2.e3ab4c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ac2032eb89c775ed0cf",
+    "url": "/reactTest2/static/js/main.a18eee12.chunk.js"
+  },
+  {
+    "revision": "2a0c0cfaccf43386bd49",
+    "url": "/reactTest2/static/js/runtime-main.1704840f.js"
+  }
+]);
